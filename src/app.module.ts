fe40164768681325ad0071fc84c5af373c02fd74
Isabelle <isabelle.taljaard@gmail.com>
2021-06-18 12:06:34 +0200

    Is committed now

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Connection, getConnectionOptions} from "typeorm";
+
+@Module({
+  imports: [
+      TypeOrmModule.forRootAsync({
+          useFactory: async () =>
+              Object.assign(await getConnectionOptions(), {
+                  autoLoadEntities: true,
+              }),
+      }),
+      UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+    constructor(private readonly connection: Connection) {
+        connection.synchronize();
+    }
+}
