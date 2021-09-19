b18ab453beaf5097c1a7ac1220d34160e92bace5
IsabelleTaljaard <83058102+IsabelleTaljaard@users.noreply.github.com>
2021-09-19 09:07:01 +0200

    18 Sept

--- src/textbooks/textbooks.service.ts
+++ src/textbooks/textbooks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Textbook } from './entities/textbook.entity';
+import { response } from 'express';
 
 @Injectable()
 export class TextbooksService {
